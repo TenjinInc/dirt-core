5b7da3d03220921959efb8a48b63901346de8db7
Robin Miller <robin@tenjin.ca>
2014-04-02 13:59:02 -0600

    Removed weird require in dirt core.

--- lib/dirt.rb
+++ lib/dirt.rb
@@ -6,7 +6,6 @@ require 'roles/validating'
 require 'roles/validation/presence_validator'
 require 'roles/validation/existence_validator'
 require 'roles/validation/self_existence_validator'
-require 'roles/validation/availability_validator'
 require 'roles/validation/closure_validator'
 
 require 'active_support/all'
